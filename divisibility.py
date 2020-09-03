88a46130edeca25cb5cf660d3bcaebbc390be776
Geoffrey Liu <main@g-liu.com>
2020-09-02 19:19:44 -0700

    This is an optimized function to check for divisibility, taking advantage of entries already in divisibility_dict

--- divisibility.py
+++ divisibility.py
@@ -2,29 +2,45 @@ import math
 import pprint
 
 
-def is_divisible(n, by):
-	return n % by == 0
-
-def main():
+class DivisibilityTester:
 	divisibility_dict = dict()
 
-	for i in range(1,1000):
-		print(i)
-		divisibility_dict[i] = []
-		upper_bound = int(math.ceil(math.sqrt(i)))
-		for j in range(1, upper_bound):
-			if is_divisible(i, j):
-				divisibility_dict[i].append(j)
-				# and add its pair
-				divisibility_dict[i].append(int(i/j))
+	def is_divisible(self, n, by):
+		return n % by == 0
 
-		divisibility_dict[i].sort()
+	def run_divisibility_check(self):
+		for i in range(1, 1000):
+			print(i)
+			self.divisibility_dict[i] = set([1, i])
 
-	with open('factors.txt', 'w') as f:
-		pp = pprint.PrettyPrinter(indent=4,stream=f)
+			j = int(math.ceil(math.sqrt(i)))
+			
 
-		pp.pprint(divisibility_dict)
+			while j >= 2:
+				is_already_known_divisor = j in self.divisibility_dict[i]
+				if not is_already_known_divisor and self.is_divisible(i, j):
+					# check our divisibility_dict to see if we have already calculated the factors for `j`
+					try:
+						factors_of_j = self.divisibility_dict[j]
 
-	print('done.')
+						for factor in factors_of_j:
+							self.divisibility_dict[i].add(factor)
+							self.divisibility_dict[i].add(int(i/factor))
 
-main()
+					except KeyError as ke:
+						self.divisibility_dict[i].add(j)
+						self.divisibility_dict[i].add(int(i/j))
+
+				j -= 1
+
+			self.divisibility_dict[i]
+
+		with open('factors.txt', 'w') as f:
+			pp = pprint.PrettyPrinter(indent=4,stream=f)
+
+			pp.pprint(self.divisibility_dict)
+
+		print('done.')
+
+# Run the code
+DivisibilityTester().run_divisibility_check()
